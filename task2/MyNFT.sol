1c97fd2c8a33445a1795b90289b0c7b80b441262
stackSg <2214434957@qq.com>
2025-08-30 23:00:51 +0800

    add task2 MYNFT

---
+++ task2/MyNFT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mintNFT(address recipient, string memory tokenURI) public returns(uint256) {
+        _tokenIds.increment();
+        uint256 id = _tokenIds.current();
+        _mint(recipient, id);
+        _setTokenURI(id, tokenURI);
+        return id;
+    }
+}
